e9eddce86aaca534e2395b74ed32316837c541e3
Niloy Mallik <niloymallik0001@gmail.com>
2026-04-30 19:33:45 +0530

    grthhrr

--- backend/express/src/models/user.models.js
+++ backend/express/src/models/user.models.js
@@ -54,19 +54,14 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   // If password is not modified or is missing (SSO users), just return
   if (!this.isModified("password") || !this.password) {
-    return next();
+    return;
   }
   
-  try {
-    // Hash the password
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-  } catch (err) {
-    next(err);
-  }
+  // Hash the password
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 // Compare password method
